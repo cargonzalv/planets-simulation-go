87cd3999a877199ca8014dd8a84e607b4cefd46a
Carlos E <cegonzalez@leal.co>
2020-06-29 15:36:50 -0500

    last tests

--- api/simulacion_test.go
+++ api/simulacion_test.go
@@ -25,3 +25,23 @@ func TestSimulacionHandler(t *testing.T) {
 		})
 	}
 }
+
+func TestSimulacionJobHandler(t *testing.T) {
+	type args struct {
+		c echo.Context
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SimulacionJobHandler(tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("SimulacionJobHandler() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
